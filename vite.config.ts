73d2e4e490ead72b445de07590f253625e0277c6
AniruddhaKalubarme <aniruddhakalubarme@gmail.com>
2026-04-24 00:55:35 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Seva_Sahayak/",
   server: {
     host: "::",
     port: 8080,
